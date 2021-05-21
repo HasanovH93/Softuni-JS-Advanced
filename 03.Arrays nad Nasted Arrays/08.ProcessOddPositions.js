5dfaf6c2efbe4654edc57066b92142b410603acf
Hasan Hasanov <hasanovh14@gmail.com>
2021-05-21 20:21:55 +0200

    Arrays and Nested arrays

--- 03.Arrays nad Nasted Arrays/08.ProcessOddPositions.js
+++ 03.Arrays nad Nasted Arrays/08.ProcessOddPositions.js
@@ -1,14 +1,19 @@
-function oddNumbers(arr){
-   
-    let newArr = []
+function oddNumbers(arr) {
+  let newArr = []
 
-    for(let i=0;i<arr.length;i++){
-        if(i % 2 === 1){
-           newArr.push(arr[i] * 2)
-        }
-    }
-    console.log(newArr.reverse())
-        
+  for(let i=0;i<arr.length;i++){
+      if(i % 2 === 1){
+         newArr.push(arr[i] * 2)
+      }
+  }
+  console.log(newArr.reverse())
+
+//   const solve = arr
+//     .filter((x, i) => i % 2)
+//     .map((x = x * 2))
+//     .reverse()
+//     .join(" ");
+//   console.log(solve);
 }
 
-oddNumbers([10, 15, 20, 25])
+oddNumbers([10, 15, 20, 25]);
