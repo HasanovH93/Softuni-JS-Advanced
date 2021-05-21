53c3789f143bad3e47605ea5714123361aedbf71
Hasan Hasanov <hasanovh14@gmail.com>
2021-05-21 20:21:55 +0200

    Arrays and Nested arrays

--- 03.Arrays nad Nasted Arrays/02.lastKNumbersSequence.js
+++ 03.Arrays nad Nasted Arrays/02.lastKNumbersSequence.js
@@ -1,8 +1,17 @@
-function lastKNumbers(n,k){
-  
-    for(let i = 0; i<=n; i++){
-        console.log(n);
+function lastKNumbers(n, k) {
+  let arr = [1];
+  for (let i = 1; i < n; i++) {
+    let sum = 0;
+
+    for (let j = arr.length - 1; j > arr.length - 1 - k; j--) {
+    
+      if (arr[j] !== undefined) {
+        sum += arr[j];
+      }
     }
+    arr.push(sum);
+  }
+  console.log(arr);
 }
 
-lastKNumbers(6,3)
+lastKNumbers(6, 3);
