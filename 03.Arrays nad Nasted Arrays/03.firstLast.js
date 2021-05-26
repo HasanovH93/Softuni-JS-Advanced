5559709864c1b4875e7c0ebf279dbcc70e7fc779
Hasan Hasanov <hasanovh14@gmail.com>
2021-05-26 20:21:55 +0200

    Arrays and Nested arrays

--- 03.Arrays nad Nasted Arrays/03.firstLast.js
+++ 03.Arrays nad Nasted Arrays/03.firstLast.js
@@ -1,10 +1,10 @@
 function firstLastNumbers(arr){
    
-    // let firstNumber = arr.shift();
-    // let secondNumber = arr.pop();
-    let firstNum = Number(arr[0]);
-    let lastNum = Number(arr[arr.length - 1]);
-    console.log(firstNum+lastNum);
-    // console.log(Number(firstNumber) + Number(secondNumber));
+    let firstNumber = arr.shift();
+    let secondNumber = arr.pop();
+    // let firstNum = Number(arr[0]);
+    // let lastNum = Number(arr[arr.length - 1]);
+    // console.log(firstNum+lastNum);
+    console.log(Number(firstNumber) + Number(secondNumber));
 }
 firstLastNumbers(["10","20","30"])
