d05046ac0cb4d6912e68593635bac20cdc3c23d2
Hasan Hasanov <hasanovh14@gmail.com>
2021-05-27 20:21:55 +0200

    Arrays and Nested arrays - Exercise

--- 04.Arrays nad Nasted Arrays - Exercise/11.diagonalAttack.js
+++ 04.Arrays nad Nasted Arrays - Exercise/11.diagonalAttack.js
@@ -1,29 +1,27 @@
 function diagonalAttack(arr) {
+  let matrix = [];
 
-   let matrix = [];
-
-   while(arr.length > 0){
-       matrix.push(arr.shift().split(" ").map(Number))
-   }
-   let primaryDiagonal = 0;
-   let secondayDiagonal = 0;
-
-   for(let i = 0; i < matrix.length; i++){
-       primaryDiagonal+= matrix[i][i];
-       secondayDiagonal+= matrix[i][matrix.length - 1 - i]
-   }
-  if(primaryDiagonal === secondayDiagonal){
-      for(let i = 0; i < matrix.length; i++){
-          for(let j = 0 ; j < matrix.length; j++){
-            if((j !== matrix.length-1-i) && (j !== i)){
-                matrix[i][j] = primaryDiagonal
-            }
-           
-          }
-      }
+  while (arr.length > 0) {
+    matrix.push(arr.shift().split(" ").map(Number));
   }
-  for(let i = 0; i < matrix.length; i++){
-      console.log(matrix[i].join(" "))
+  let primaryDiagonal = 0;
+  let secondayDiagonal = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    primaryDiagonal += matrix[i][i];
+    secondayDiagonal += matrix[i][matrix.length - 1 - i];
+  }
+  if (primaryDiagonal === secondayDiagonal) {
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix.length; j++) {
+        if (j !== matrix.length - 1 - i && j !== i) {
+          matrix[i][j] = primaryDiagonal;
+        }
+      }
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    console.log(matrix[i].join(" "));
   }
 }
 
