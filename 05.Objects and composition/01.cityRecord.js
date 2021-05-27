dab71a19ffd621108395b146063ba4ae14a813fd
Hasan Hasanov <hasanovh14@gmail.com>
2021-05-27 20:21:55 +0200

    Objects and Composition

---
+++ 05.Objects and composition/01.cityRecord.js
@@ -0,0 +1,10 @@
+function cityRecord(name, population, treasury) {
+  let cityRecord = {
+    name,
+    population,
+    treasury,
+  };
+  console.log(cityRecord);
+}
+
+cityRecord("Tortuga", 7000, 15000);
