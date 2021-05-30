c6c73ac4cd606858cd9cd194eac7a4c5bffc7e30
Hasan Hasanov <hasanovh14@gmail.com>
2021-05-30 15:15:21 +0200

    Objects and Composition - Exercise

---
+++ 06.Objects and composition - Exercise/02.constructionCrew.js
@@ -0,0 +1,15 @@
+function constructionCrew(worker) {
+  if (worker.dizziness) {
+    worker.levelOfHydrated += 0.1 * worker.weight * worker.experience;
+  }
+
+  return worker;
+}
+console.log(
+  constructionCrew({
+    weight: 120,
+    experience: 20,
+    levelOfHydrated: 200,
+    dizziness: true,
+  })
+);
