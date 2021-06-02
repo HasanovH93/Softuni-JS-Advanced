6152cf841185e9a3f7801648b0a6c88e9d32a629
Hasan Hasanov <hasanovh14@gmail.com>
2021-06-02 15:15:21 +0200

    Objects and Composition - Exercise

--- 06.Objects and composition - Exercise/03.carFactory.js
+++ 06.Objects and composition - Exercise/03.carFactory.js
@@ -1,38 +1,69 @@
-function carFactory(obj) {
-  const result = {};
 
-  let engine = {};
-  let carriage = {};
+//Solution 1
+// function carFactory(obj) {
+//   const result = {};
 
-  result.model = obj.model;
-  if (obj.power <= 90) {
-    engine.power = 90;
-    engine.volume = 1800;
-  } else if (obj.power <= 120) {
-    engine.power = 120;
-    engine.volume = 2400;
-  } else if (obj.power <= 200) {
-    engine.power = 200;
-    engine.volume = 3500;
-  }
+//   let engine = {};
+//   let carriage = {};
 
-  carriage.type = obj.carriage;
-  carriage.color = obj.color;
+//   result.model = obj.model;
+//   if (obj.power <= 90) {
+//     engine.power = 90;
+//     engine.volume = 1800;
+//   } else if (obj.power <= 120) {
+//     engine.power = 120;
+//     engine.volume = 2400;
+//   } else if (obj.power <= 200) {
+//     engine.power = 200;
+//     engine.volume = 3500;
+//   }
 
-  if (obj.wheelsize % 2 === 0) {
-    obj.wheelsize--;
-  }
+//   carriage.type = obj.carriage;
+//   carriage.color = obj.color;
 
-  result.engine = engine;
-  result.carriage = carriage;
-  result.wheels = [obj.wheelsize, obj.wheelsize, obj.wheelsize, obj.wheelsize];
+//   if (obj.wheelsize % 2 === 0) {
+//     obj.wheelsize--;
+//   }
 
-  return result;
+//   result.engine = engine;
+//   result.carriage = carriage;
+//   result.wheels = [obj.wheelsize, obj.wheelsize, obj.wheelsize, obj.wheelsize];
+
+//   return result;
+// }
+// carFactory({
+//   model: "VW Golf II",
+//   power: 90,
+//   color: "blue",
+//   carriage: "hatchback",
+//   wheelsize: 14,
+// });
+
+// solution 2
+
+function solve(obj) {
+  let engines = [
+    { power: 90, volume: 1800 },
+    { power: 120, volume: 2400 },
+    { power: 200, volume: 3500 },
+  ];
+  let carriages = [
+    { type: "hatchback", color: obj.color },
+    { type: "coupe", color: obj.color },
+  ];
+  let wheelSize = obj.wheelSize % 2 == 1 ? obj.wheelSize : obj.wheelSize - 1;
+
+  return {
+    model: obj.model,
+    engine: engines.filter(e => e.power >= obj.power)[0],
+    carriages : carriages.filter(c => c.type == obj.carriage)[0],
+    wheels: [wheelSize,wheelSize,wheelSize,wheelSize]
+  };
 }
-carFactory({
+console.log(solve({
   model: "VW Golf II",
   power: 90,
   color: "blue",
   carriage: "hatchback",
   wheelsize: 14,
-});
+}));
