cefb3b3e7df90a1d67a8a17020ea54b156716990
Hasan Hasanov <hasanovh14@gmail.com>
2021-06-02 15:15:21 +0200

    Objects and Composition - Exercise

--- 06.Objects and composition - Exercise/05.lowestPriceInCities.js
+++ 06.Objects and composition - Exercise/05.lowestPriceInCities.js
@@ -1,26 +1,61 @@
-function lowestPrice(arr) {
-  let products = [];
+//Solution 1
+// function lowestPrice(arr) {
+//   let products = [];
 
-  while (arr.length > 0) {
-    let [town, product, price] = arr.shift().split(" | ");
-    price = Number(price);
+//   while (arr.length > 0) {
+//     let [town, product, price] = arr.shift().split(" | ");
+//     price = Number(price);
 
-    if (products.filter((x) => x.product === product).length > 0) {
-      let obj = products.find((x) => x.product === product);
-      if (obj.price > price) {
-        obj.price = price;
-        obj.town = town;
-      }
+//     if (products.filter((x) => x.product === product).length > 0) {
+//       let obj = products.find((x) => x.product === product);
+//       if (obj.price > price) {
+//         obj.price = price;
+//         obj.town = town;
+//       }
+//     } else {
+//       let obj = { product, town, price };
+//       products.push(obj);
+//     }
+//   }
+//   for (const product of products) {
+//     console.log(`${product.product} -> ${product.price} (${product.town})`);
+//   }
+// }
+// lowestPrice([
+//   "Sample Town | Sample Product | 1000",
+//   "Sample Town | Orange | 2",
+//   "Sample Town | Peach | 1",
+//   "Sofia | Orange | 3",
+//   "Sofia | Peach | 2",
+//   "New York | Sample Product | 1000.1",
+//   "New York | Burger | 10",
+// ]);
+
+//Solution 2
+
+function solve(arr) {
+  let obj = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    let line = arr[i];
+    let [town, product, price] = line.split(" | ");
+    price = Number(price)
+
+    if (!obj.hasOwnProperty(product)) {
+      obj[product] = { town, price };
     } else {
-      let obj = { product, town, price };
-      products.push(obj);
+      if (price <= obj[product].price) {
+        obj[product] = { town, price };
+      }
     }
   }
-  for (const product of products) {
-    console.log(`${product.product} -> ${product.price} (${product.town})`);
+  let productNames = Object.keys(obj);
+  for (const product of productNames) {
+    console.log(`${product} => ${obj[product].price} (${obj[product].town})`)
   }
 }
-lowestPrice([
+
+solve([
   "Sample Town | Sample Product | 1000",
   "Sample Town | Orange | 2",
   "Sample Town | Peach | 1",
