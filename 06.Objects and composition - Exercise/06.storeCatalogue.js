2d962dd32513ac89510f6c16cb048d9f8023d919
Hasan Hasanov <hasanovh14@gmail.com>
2021-06-03 15:15:21 +0200

    Objects and Composition - Exercise

--- 06.Objects and composition - Exercise/06.storeCatalogue.js
+++ 06.Objects and composition - Exercise/06.storeCatalogue.js
@@ -1,17 +1,59 @@
-function store(arr) {
-  let filtered = arr.sort((a, b) => a.localeCompare(b));
-  let current = [];
-  let char = "";
-  for (let i = 0; i < filtered.length; i++) {
-    current = filtered[i].split(" : ");
-    if (filtered[i][0] != char) {
-      console.log(filtered[i][0]);
-      char = filtered[i][0];
+//Solution 1
+
+// function store(arr) {
+//   let filtered = arr.sort((a, b) => a.localeCompare(b));
+//   let current = [];
+//   let char = "";
+//   for (let i = 0; i < filtered.length; i++) {
+//     current = filtered[i].split(" : ");
+//     if (filtered[i][0] != char) {
+//       console.log(filtered[i][0]);
+//       char = filtered[i][0];
+//     }
+//     console.log(`  ${current[0]}: ${current[1]}`);
+//   }
+// }
+// store([
+//   "Appricot : 20.4",
+//   "Fridge : 1500",
+//   "TV : 1499",
+//   "Deodorant : 10",
+//   "Boiler : 300",
+//   "Apple : 1.25",
+//   "Anti-Bug Spray : 15",
+//   "T-Shirt : 10",
+// ]);
+
+//Solution 2
+
+function solve(arr) {
+  let obj = {};
+
+  for (const line of arr) {
+    let [product, price] = line.split(" : ");
+    price = Number(price);
+    let letter = product[0];
+
+    if (!obj[letter]) {
+      obj[letter] = {};
     }
-    console.log(`  ${current[0]}: ${current[1]}`);
+    obj[letter][product] = price;
   }
+
+  let sortedLetters = Object.keys(obj).sort((a, b) => a.localeCompare(b));
+  for (const letter of sortedLetters) {
+    console.log(letter);
+    let sortedProducts = Object.keys(obj[letter]).sort((a, b) =>
+      a.localeCompare(b)
+    );
+    for (const product of sortedProducts) {
+      console.log(`  ${product}: ${obj[letter][product]}`);
+    }
+  }
+  console.log(obj);
 }
-store([
+
+solve([
   "Appricot : 20.4",
   "Fridge : 1500",
   "TV : 1499",
---
+++ 06.Objects and composition - Exercise/07.townsToJSON.js
@@ -0,0 +1,31 @@
+function townsToJSON(arr) {
+  let result = [];
+
+  class Town {
+    constructor(name, latitude, longtitude) {
+      (this.Town = name),
+        (this.Latitude = Number(latitude)), /// Bad Practice
+        (this.Longitude = Number(longtitude));
+    }
+  }
+
+  let regexp = /\s*\|\s*/gm;
+  for (let i = 1; i < arr.length; i++) {
+    let newArr = arr[i].split(regexp).filter((x) => x.length != 0); //.map(t => t.trim()).filter(x =>x.length != 0);// without regex
+
+    let [townName, latitude, longitude] = newArr;
+
+    latitude = Number(latitude).toFixed(2);
+    longitude = Number(longitude).toFixed(2);
+
+    let town = new Town(townName, latitude, longitude);
+    result.push(town);
+  }
+  return JSON.stringify(result)
+}
+
+console.log(townsToJSON([
+  "| Town | Latitude | Longitude |",
+  "| Sofia | 42.696552 | 23.32601 |",
+  "| Beijing | 39.913818 | 116.363625 |",
+]));
