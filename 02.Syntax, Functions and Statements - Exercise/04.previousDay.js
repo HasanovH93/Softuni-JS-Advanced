c20c7c0a4a93a2fc199048dfb0bb928bbfe5c296
Hasan Hasanov <hasanovh14@gmail.com>
2021-06-04 20:21:55 +0200

    Basic Syntax

--- 02.Syntax, Functions and Statements - Exercise/04.previousDay.js
+++ 02.Syntax, Functions and Statements - Exercise/04.previousDay.js
@@ -1,6 +1,6 @@
-function prevDay(year,month,day){
- 
-   let date = new Date(year,month,day - 1 );
-   console.log(date)
+function prevDay(year, month, day) {
+  const date = new Date(year, month - 1, day);
+  date.setDate(date.getDate() - 1);
+  console.log(`${date.getFullYear()}-${date.getMonth() +1}-${date.getDate()}`)
 }
-prevDay(2016, 9, 1)
+prevDay(2016, 9, 1);
