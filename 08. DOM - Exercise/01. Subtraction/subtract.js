5c6cdbecd22cede2df2e527d48e15731d5c6e66c
Hasan Hasanov <hasanovh14@gmail.com>
2021-06-07 13:21:55 +0200

    DOM Exercise

--- 08. DOM - Exercise/01. Subtraction/subtract.js
+++ 08. DOM - Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,11 @@
 function subtract() {
-    console.log('TODO:...');
+    let firstNum = document.getElementById('firstNumber');
+    let firstAsNum = Number(firstNum.value)
+    let secondNum = document.getElementById('secondNumber');
+    let secondAsNum = Number(secondNum.value)
+    const result = firstAsNum - secondAsNum
+
+   let resultElement =  document.getElementById('result');
+
+   resultElement.textContent = result
 }
