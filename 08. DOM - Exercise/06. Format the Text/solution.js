1ef1466a0c399e46e6a2e5704f8e76050af14063
Hasan Hasanov <hasanovh14@gmail.com>
2021-06-08 23:13:25 +0200

    DOM Exercise

--- 08. DOM - Exercise/06. Format the Text/solution.js
+++ 08. DOM - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,15 @@
 function solve() {
-  //TODO
+  let textArea = document.getElementById("input");
+  let textArr = textArea.value.split(".").filter((s) => s !== "");
+  let output = document.getElementById("output");
+
+  while(textArr.length > 0){
+
+    let text = textArr.splice(0,3).join('. ') + '.';
+    let p = document.createElement('p');
+    p.textContent = text
+
+    output.appendChild(p)
+
+  }
 }
