2a2788fd86be40d1a023928c9328e0edcb59e5ec
Hasan Hasanov <hasanovh14@gmail.com>
2021-06-09 23:13:25 +0200

    DOM Exercise

--- 09.DOM Manipulation and Events/02. Delete-From-Table/app.js
+++ 09.DOM Manipulation and Events/02. Delete-From-Table/app.js
@@ -1,3 +1,19 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  const inputValue = document.querySelector('input[name="email"]').value;
+
+  const rows = [...document.querySelectorAll("tbody tr")];
+
+  let isFound = false;
+  for (const row of rows) {
+    if (row.children[1].textContent === inputValue) {
+      const parent = row.parentElement;
+      parent.removeChild(row);
+      isFound = true;
+      break;
+    }
+  }
+
+
+    document.getElementById("result").textContent =  isFound ? "Deleted." : "Not found.";
+  
 }
