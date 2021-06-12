84764eace718e959e09ea01b57ff5c57d0e91e84
Hasan Hasanov <hasanovh14@gmail.com>
2021-06-12 14:15:34 +0200

    DOM Exercise

--- 10.DOM Manipulation and Events - Exercise/01. Sections/script.js
+++ 10.DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,24 @@
-function create(words) {
-   console.log('TODO:...');
+function create(input) {
+   let parentElement = document.getElementById('content');
+   let elements = input;
+   
+   for(let i = 0; i < elements.length; i++){
+        let div = document.createElement('div');
+        let p = document.createElement('p');
+
+        let currText = document.createTextNode(elements[i]);
+
+        p.appendChild(currText)
+        p.style.display = 'none';
+        div.appendChild(p)
+        
+        div.addEventListener('click', showContent)
+        parentElement.appendChild(div)
+   }
+
+
+   function showContent(event){
+      event.target.children[0].style.display = 'inline';
+   }
+
 }
