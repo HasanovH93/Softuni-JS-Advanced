bc57883a430f6a7ffb12244bbc32724795c679ad
Hasan Hasanov <hasanovh14@gmail.com>
2021-06-14 20:21:55 +0200

    DOM

--- 10.DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ 10.DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,21 @@
 function addItem() {
-    console.log('TODO:...');
+    
+
+    let newText = document.getElementById('newItemText').value;
+    let newValue = document.getElementById('newItemValue').value;
+
+    let option = document.createElement('option');
+    option.text = newText;
+    option.value = newValue;
+
+    let dropDownMenu = document.getElementById('menu');
+    if(newText !== '' && newValue !== '') {
+        dropDownMenu.appendChild(option);
+    }
+   
+    
+
+    document.getElementById('newItemText').value = '';
+    document.getElementById('newItemValue').value = '';
+
 }
