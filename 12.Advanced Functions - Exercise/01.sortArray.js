3e4353822bc722a93baca86ecd63aee722311d04
Hasan Hasanov <hasanovh14@gmail.com>
2021-06-16 20:21:55 +0200

    Advanced Functions - Exercise

---
+++ 12.Advanced Functions - Exercise/01.sortArray.js
@@ -0,0 +1,12 @@
+function sortArr(arr, type) {
+  if (type === "asc") {
+    arr.sort((a, b) => a - b);
+  } else {
+    arr.sort((a, b) => b - a);
+  }
+
+  return arr;
+}
+
+sortArr([14, 7, 17, 6, 8], "asc");
+sortArr([14, 7, 17, 6, 8], "desc");
