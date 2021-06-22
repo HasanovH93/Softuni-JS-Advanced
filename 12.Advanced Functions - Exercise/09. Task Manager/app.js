28ae21c714ee4190ee17f0d9e1abefac1ebc800e
Hasan Hasanov <hasanovh14@gmail.com>
2021-06-22 20:21:55 +0200

    Advanced Functions - Exercise

--- 12.Advanced Functions - Exercise/09. Task Manager/app.js
+++ 12.Advanced Functions - Exercise/09. Task Manager/app.js
@@ -1,9 +1,46 @@
 function solve() {
+    const input = {
+        name: document.getElementById("task"),
+        description: document.getElementById("description"),
+        date: document.getElementById("date"),
+    };
 
-    document.getElementById('add').addEventListener('click',addTask)
+    const [_,openSection,progressSection,finishSection] = [...document.querySelectorAll('section')]
 
-    function addTask(event){
-        event.preventDefault()
+    document.getElementById("add").addEventListener("click", addTask);
+
+    function addTask(event) {
+        event.preventDefault();
+
+        //collect field values
+        //create elements
+        const article = document.createElement("article");
+        article.appendChild(createElement("h3", input.name.value));
+        article.appendChild(
+            createElement("p", `Desciption: ${input.description.value}`)
+        );
+        article.appendChild(createElement("p", `Due date: ${input.date.value}`));
+        const div = createElement('div', "", 'flex');
+        div.appendChild(createElement('button','Start','green'))
+        div.appendChild(createElement('button','Delete','red'))
+        article.appendChild(div)
+        //append to Open Secion
+
+        openSection.appendChild('article')
+        
+        Object.values(input).forEach(i => i.vaue = '')
+
+
+
+        // ** add functionaloty for start , finish ,delete task.
     }
-    
+
+    function createElement(type, content, className) {
+        const element = document.createElement(type);
+        element.textContent = content;
+        if (className) {
+            element.className = className;
+        }
+        return element;
+    }
 }
