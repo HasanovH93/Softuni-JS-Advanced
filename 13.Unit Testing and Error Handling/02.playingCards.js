5ab740c0163132686b0d61f884e027d65f8bbfb7
Hasan Hasanov <hasanovh14@gmail.com>
2021-06-25 20:21:55 +0200

    Advanced Functions - Exercise

--- 13.Unit Testing and Error Handling/02.playingCards.js
+++ 13.Unit Testing and Error Handling/02.playingCards.js
@@ -14,17 +14,22 @@ function createCard(face, suit) {
     "K",
     "A",
   ];
-
-  if(faces.indexOf(face) == -1){
-    throw new Error('Invalid face: ' + face)
-  }
-
   const suits = {
     S: "\u2660",
     H: "\u2665",
     D: "\u2666",
     C: "\u2663",
   };
+
+  if(faces.indexOf(face) == -1){
+    throw new Error('Invalid face: ' + face)
+  }
+
+  if(suits[suit] == undefined){
+    throw new Error('Invalid suit: ' + face)
+  }
+
+ 
 
   const result = {
     face,
