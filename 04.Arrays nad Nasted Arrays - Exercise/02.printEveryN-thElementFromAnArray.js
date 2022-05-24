f571fbe042a330734e76785fbcfa6182b7540f1b
Hasan Hasanov <hasanovh14@gmail.com>
2022-05-24 18:00:10 +0200

    Arrays Exercise

---
+++ 04.Arrays nad Nasted Arrays - Exercise/02.printEveryN-thElementFromAnArray.js
@@ -0,0 +1,20 @@
+function printEveryN(arr,n) {
+   
+//    let filtered = arr.filter((el,i)=> {
+//        if(i % 2 === 0){
+//            console.log(el)
+//        }
+//    })
+   
+
+    let newArr = []
+   for(let i = 0; i< arr.length; i++){
+       if(i % n == 0){
+           newArr.push(arr[i])
+       }
+   }
+   return newArr
+
+}
+
+console.log(printEveryN(["5", "20", "31", "4", "20"], 2));
