9cb830dd7dfb974bca024e5e6a97bf051e679dab
Hasan Hasanov <hasanovh14@gmail.com>
2022-05-30 11:33:00 +0200

    DOM

---
+++ 07.DOM introduction/02.Collect list Items/collectListItems.js
@@ -0,0 +1,8 @@
+function extractText(){
+    const items = Array.from(document.querySelectorAll('li'))
+    const result = items.map(e => e.textContent).join('\n')
+
+    document.getElementById('result').value = result
+   
+    
+}
