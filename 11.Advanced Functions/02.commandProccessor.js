0ed03627ee5955f5c823c3b5c21195594587c587
Hasan Hasanov <hasanovh14@gmail.com>
2022-06-15 12:12:28 +0200

    Advanced Functions

---
+++ 11.Advanced Functions/02.commandProccessor.js
@@ -0,0 +1,39 @@
+function createProccessor(){
+
+            let state = '';
+
+            function append(str){
+                state += str
+            }
+            function removeStart(n){
+                state = state.slice(n)
+            }
+
+            function removeEnd(n){
+                state = state.slice(0,-n)
+            }
+            function print(){
+                console.log(state)
+            }
+           return {
+            append,
+            removeStart,
+            removeEnd,
+            print
+           }
+}
+
+
+
+let firstZeroTest = createProccessor();
+
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
+
+
+
+
+createProccessor()
