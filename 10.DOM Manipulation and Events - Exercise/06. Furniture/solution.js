eb42b13ab878b1c8f13a9e85c89a1bf22796d729
Hasan Hasanov <hasanovh14@gmail.com>
2022-06-15 13:36:31 +0200

    DOM,Advanced Functions

--- 10.DOM Manipulation and Events - Exercise/06. Furniture/solution.js
+++ 10.DOM Manipulation and Events - Exercise/06. Furniture/solution.js
@@ -1,4 +1,81 @@
 function solve() {
+  const [generateBtn, buyBtn] = Array.from(document.querySelectorAll("button"));
+  const [input, output] = Array.from(document.querySelectorAll("textarea"));
+  const tbody = document.querySelector("tbody");
 
-  //TODO...
+  generateBtn.addEventListener("click", generate);
+  buyBtn.addEventListener('click', buy)
+
+  const items = [];
+
+  function generate() {
+    const data = JSON.parse(input.value);
+
+    for (let item of data) {
+      const row = document.createElement("tr");
+      // Image Column
+      row.appendChild(createColumn("img", item.img));
+      // Name Column
+      row.appendChild(createColumn("p", item.name));
+      // price Column
+      row.appendChild(createColumn("p", item.price));
+      //Decoration Column
+      row.appendChild(createColumn("p", item.decFactor));
+
+      //input Column
+      const c5 = document.createElement("td");
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+
+      c5.appendChild(checkbox);
+      row.appendChild(c5);
+
+      tbody.appendChild(row);
+
+      items.push({
+        ...item,
+        isChecked
+      })
+      function isChecked(){
+        return checkbox.checked
+      }
+    }
+  }
+  function buy(){
+    let list = [];
+    let total = 0;
+    let decoration = 0;
+    const boughtItems = items
+    .filter(i => i.isChecked());
+
+    for(let item of boughtItems){
+      list.push(item.name)
+      total+= Number(item.price)
+      decoration += Number(item.decFactor)
+    }
+
+    decoration /= boughtItems.length
+
+    output.value = [
+      `Bought furniture: ${list.join(', ')}`,
+      `Total price: ${total.toFixed(2)}`,
+      `Average decoration factor: ${decoration}`
+    ].join('\n')
+   
+
+  }
+
+  function createColumn(type, content) {
+    const result = document.createElement("td");
+    let inner;
+    if (type == "img") {
+      inner = document.createElement("img");
+      inner.src = content;
+    } else {
+      inner = document.createElement("p");
+      inner.textContent = content;
+    }
+    result.appendChild(inner);
+    return result;
+  }
 }
