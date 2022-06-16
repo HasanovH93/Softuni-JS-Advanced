8bc1a569610d486c24c4d8d29e6591378aac22f5
Hasan Hasanov <hasanovh14@gmail.com>
2022-06-16 11:08:34 +0200

    Advanced Functions - DOM

--- 12.Advanced Functions - Exercise/08. Next Article/script.js
+++ 12.Advanced Functions - Exercise/08. Next Article/script.js
@@ -1,3 +1,14 @@
 function getArticleGenerator(articles) {
-    // TODO
+
+    const div = document.getElementById('content')
+
+    return function() {
+        const article = articles.shift()
+        const element = document.createElement('article');
+        element.textContent = article
+
+        div.appendChild(element)
+    }
+
+   
 }
