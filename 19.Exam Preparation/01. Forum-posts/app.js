d5c9506e74dca13e669fa8e8e732175dc1cc546d
Hasan Hasanov <hasanovh14@gmail.com>
2022-06-24 19:32:59 +0200

    Exam Preparation

--- 19.Exam Preparation/01. Forum-posts/app.js
+++ 19.Exam Preparation/01. Forum-posts/app.js
@@ -1,115 +1,75 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  function solve() {
-    const titleInputEl = document.getElementById("post-title");
-    const categoryInputEl = document.getElementById("post-category");
-    const contentInputEl = document.getElementById("post-content");
-    const reviewUlEl = document.getElementById("review-list");
-    const publishedUlEl = document.getElementById("published-list");
-    const reviewMessage = document.getElementById("reviewed-msg");
-    const uploadedMessage = document.getElementById("approved-msg");
-    document.getElementById("clear-btn").addEventListener("click", clearPosts);
-    const submitButtonEl = document
-      .getElementById("publish-btn")
-      .addEventListener("click", (ev) => {
-        if (
-          titleInputEl.value !== "" &&
-          categoryInputEl.value !== "" &&
-          contentInputEl.value !== ""
-        ) {
-          addPost(
-            ev,
-            titleInputEl.value,
-            categoryInputEl.value,
-            contentInputEl.value
-          );
-          clearInputFileds();
-        }
-      });
+  const input = {
+    title: document.getElementById("post-title"),
+    category: document.getElementById("post-category"),
+    content: document.getElementById("post-content"),
+  };
+  const lists = {
+    review: document.getElementById("review-list"),
+    published: document.getElementById("published-list"),
+  };
 
-    function addPost(ev, titleInputEl, categoryInputEl, contentInputEl) {
-      // ev.preventDefault();
+  document.getElementById("publish-btn").addEventListener("click", publish);
+  document.getElementById("clear-btn").addEventListener("click", clear);
 
-      const li = elGenerator("li");
-      li.setAttribute("class", "rpost");
-      const article = elGenerator("article", "", li);
-      elGenerator("h4", `${titleInputEl}`, article);
-      elGenerator("p", `Category: ${categoryInputEl}`, article);
-      elGenerator("p", `Content: ${contentInputEl}`, article);
+  function publish() {
+    //read input fields
+    const title = input.title.value;
+    const category = input.category.value;
+    const content = input.content.value;
 
-      const approveBtn = elGenerator("button", "Approve", li);
-      approveBtn.setAttribute("class", "action-btn approve");
-      // approveBtn.setAttribute("class", "approve");
-      const editBtn = elGenerator("button", "Edit", li);
-      editBtn.setAttribute("class", "action-btn edit");
-      // editBtn.setAttribute("class", "edit");
+    //validate input
 
-      reviewUlEl.appendChild(li);
-
-      if (reviewUlEl.children.length !== 0) {
-        reviewMessage.style.display = "none";
-      }
-
-      approveBtn.addEventListener("click", (ev) => approvePost(ev, article));
-      editBtn.addEventListener("click", (ev) =>
-        editPost(ev, titleInputEl, categoryInputEl, contentInputEl)
-      );
+    if (title == "" || category == "" || content == "") {
+      return;
     }
 
-    function editPost(ev, _titleInputEl, _categoryInputEl, _contentInputEl) {
-      ev.target.parentNode.remove();
+    //create list item
+    const li = document.createElement("li");
+    li.className = "rpost";
+    li.innerHTML = `<article>
+   <h4>${title}</h4>
+   <p>Category: ${category}</p>
+   <p>Content: ${content}</p>
+   </article>
+   <button class ="action-btn edit">Edit</button>
+   <button class ="action-btn approve">Approve</button>`;
 
-      titleInputEl.value = _titleInputEl;
-      categoryInputEl.value = _categoryInputEl;
-      contentInputEl.value = _contentInputEl;
+    // *add functionality to buttons
+
+    const editBtn = li.querySelector(".edit");
+    const approveBtn = li.querySelector(".approve");
+
+    editBtn.addEventListener("click", edit);
+    approveBtn.addEventListener("click", approve);
+    //appent to first list
+    lists.review.appendChild(li);
+
+    //clear input
+    input.title.value = "";
+    input.category.value = "";
+    input.content.value = "";
+
+    function edit() {
+      input.title.value = title;
+      input.category.value = category;
+      input.content.value = content;
+      // remove list item from list
+      li.remove();
     }
+    function approve() {
+      lists.published.appendChild(li);
 
-    function approvePost(ev, articleEl) {
-      ev.target.parentNode.remove();
+      // remove action buttons
 
-      const publishedLiEl = document.createElement("li");
-      publishedLiEl.className = "rpost";
-      publishedLiEl.appendChild(articleEl);
-      publishedUlEl.appendChild(publishedLiEl);
-
-      if (publishedUlEl.children.length !== 0) {
-        uploadedMessage.style.display = "none";
-      }
-
-      if (reviewUlEl.children.length == 0) {
-        reviewMessage.style.display = "block";
-      }
+      editBtn.remove();
+      approveBtn.remove();
     }
+  }
 
-    function clearPosts() {
-      // if (publishedUlEl.children.length > 0) {
-      //   publishedUlEl.innerHTML = "";
-      //   uploadedMessage.style.display = "block";
-      // }
-      let postsArray = Array.from(publishedUlEl.children);
-      postsArray.forEach((post) => {
-        post.remove();
-      });
-      if (publishedUlEl.children.length == 0) {
-        uploadedMessage.style.display = "block";
-      }
-    }
-
-    function clearInputFileds() {
-      titleInputEl.value = "";
-      categoryInputEl.value = "";
-      contentInputEl.value = "";
-    }
-
-    function elGenerator(type, content, parent) {
-      const element = document.createElement(type);
-      element.textContent = content;
-
-      if (parent) {
-        parent.appendChild(element);
-      }
-      return element;
-    }
+  function clear() {
+   lists.published.innerHTML = ''
   }
 }
